1080fc44de13b0b7710fe46a782d4a54f1aece16
neville777 <neville.james@student.moringaschool.com>
2023-10-13 08:29:45 +0300

    styling with css

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const apiUrl = "https://api.nasa.gov/planetary/apod";
+const apiUrl = " https://api.nasa.gov/planetary/apod";
+const api ="Gpmv5R1qrkXRrhgfoNt5loyMZXOjREcuhs4GqJwk"; 
 const worldImage = document.getElementById("world");
 const descriptionOne = document.getElementById("explanationOne");
 const datePicker = document.getElementById("datePicker");
@@ -7,69 +8,70 @@ const fullScreenButton = document.getElementById("fullScreenButton");
 const comments = document.getElementById("comments"); 
 
 // Array to store the comment
-
 const commentsArr = [];
 
 // To generalize HTML list based on the comments array
-const displayComments = ()=> {
-    let list = ``;
+const displayComments = () => {
+    let list = '';
     commentsArr.forEach(comment => {
         list += `<li class="comment-item">${comment}</li>`;
     });
-    list += `</li>`;
+    list += '</li>';
     comments.innerHTML = list;
 };
 
 clear.onclick = function (e) {
     e.preventDefault();
-
     // Reset the array
-    commentsArr.lenght = 0;
+    commentsArr.length = 0;
 
-    //Re-generate the comment HTML list
+    // Re-generate the comment HTML list
     displayComments();
 };
 
-SubmitEvent.onclick = function (e) {
+submit.onclick = function (e) {
     e.preventDefault();
     const field = document.getElementById("field");
     const content = field.value;
-    if (content.lenght > 0) {
+    if (content.length > 0) {
         commentsArr.push(content);
         displayComments();
-        field.value=  ``;
+        field.value = '';
     }
-}
+};
 
-let currentDate = new Date().toISOString().split('')[0];
+let currentDate = new Date().toISOString().split('T')[0];
 
 fetchButton.addEventListener('click', fetchImage);
 fullScreenButton.addEventListener('click', toggleFullScreen);
 
-function fetchImage() {
-    const selectDate = datePicker.value;
 
-    if(selectDate) {
-        fetch( ` ${apiUrl}?api_key=${apiKey}&date=${selectDate} `)
-          .then(response => response.json())
-          .then((data) => {
-            displayImage(data);
-          }) 
-          .catch((error) => {
-            alert(`ERROR FETCHING IMAGE`,error);
-        });
+function fetchImage() {
+    const selectedDate = datePicker.value;
+
+    if (selectedDate) {
+        fetch(`${apiUrl}?api_key=${api}&date=${selectedDate}`)
+            .then((response) => response.json())
+            .then((data) => {
+                displayImage(data);
+            })
+            .catch((error) => {
+                alert(`Error fetching NASA image`, error);
+            });
     } else {
-        alert("Please select a date.");
+        alert('Please select a date.');
     }
 }
+
 function displayImage(data) {
-    worldImage.scroll = data.url;
+    worldImage.src = data.url;
     descriptionOne.textContent = data.explanation;
 }
 
 function toggleFullScreen() {
     const element = worldImage;
-    element.requestFullscreen();
+   element.requestFullscreen();
 }
 
-// Initial load 
+// Initial load with today's date
+fetchImage();
